5e9cc2f7ffb2481ff5f5b0111f26dd25de919496
Rafael Tavares <rafaeltavares91@gmail.com>
2026-04-12 00:41:45 -0300

    chore: initialize project structure

---
+++ src/test/java/io/github/rafaeltavares/DistributedHighThroughputRateLimiterTest.java
@@ -0,0 +1,13 @@
+package io.github.rafaeltavares;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DistributedHighThroughputRateLimiterTest {
+
+    @Test
+    void shouldRunTestSuite() {
+        assertTrue(true);
+    }
+}
